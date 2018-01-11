4e17e08850f0c294f150f532e549ab8dd05121de
Mathis Pandelé <mapandel@student.42.fr>
2018-01-11 05:15:55 +0100

    Sully.c 1.0 : Implementation

---
+++ Sully.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main()
+{
+	int i = 5;
+	if (i <= 0)
+		return (0);
+	char *fname[18];
+	sprintf((char*)fname, "Sully_%d.c", i - 1);
+	char *exname[16];
+	sprintf((char*)exname, "Sully_%d", i - 1);
+	int fd = open((char*)fname, O_WRONLY | O_CREAT, 0777);
+	char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$c%1$cint main()%1$c{%1$c%2$cint i = %6$d;%1$c%2$cif (i <= 0)%1$c%2$c%2$creturn (0);%1$c%2$cchar *fname[18];%1$c%2$csprintf((char*)fname, %3$cSully_%5$cd.c%3$c, i - 1);%1$c%2$cchar *exname[16];%1$c%2$csprintf((char*)exname, %3$cSully_%5$cd%3$c, i - 1);%1$c%2$cint fd = open((char*)fname, O_WRONLY | O_CREAT, 0777);%1$c%2$cchar *s = %3$c%4$s%3$c;%1$c%2$cdprintf(fd, s, 10, 9, 34, s, 37, i - 1);%1$c%2$cchar *cmd[46];%1$c%2$csprintf((char*)cmd, %3$cclang -o %5$c2$s %5$c1$s; ./%5$c2$s%3$c, (char*)fname, (char*)exname);%1$c%2$csystem((char*)cmd);%1$c}%1$c";
+	dprintf(fd, s, 10, 9, 34, s, 37, i - 1);
+	char *cmd[46];
+	sprintf((char*)cmd, "clang -o %2$s %1$s; ./%2$s", (char*)fname, (char*)exname);
+	system((char*)cmd);
+}
