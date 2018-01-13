a240815e6761838311862d780ce887464075a055
Mathis Pandelé <mapandel@student.42.fr>
2018-01-13 05:36:17 +0100

    Colleen.c 1.1 : Fit to the subject

--- Colleen.c
+++ Colleen.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 void func(){}
-//1
+/*
+	1
+*/
 int main()
-{ //2
+{
+	/*
+		2
+	*/
 	func();
-	char *s = "#include <stdio.h>%1$cvoid func(){}%1$c//1%1$cint main()%1$c{ //2%1$c%2$cfunc();%1$c%2$cchar *s = %3$c%4$s%3$c;%1$c%2$cprintf(s, 10, 9, 34, s);%1$c}%1$c";
+	char *s = "#include <stdio.h>%1$cvoid func(){}%1$c/*%1$c%2$c1%1$c*/%1$cint main()%1$c{%1$c%2$c/*%1$c%2$c%2$c2%1$c%2$c*/%1$c%2$cfunc();%1$c%2$cchar *s = %3$c%4$s%3$c;%1$c%2$cprintf(s, 10, 9, 34, s);%1$c}%1$c";
 	printf(s, 10, 9, 34, s);
 }
